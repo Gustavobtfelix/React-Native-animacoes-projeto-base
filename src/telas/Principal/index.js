4c68b9706707024a576f25995489502e601da441
Gustavobtfelix <90421959+Gustavobtfelix@users.noreply.github.com>
2022-10-11 12:03:40 -0300

    Adicionando animação de carregamento
    Shimmer Effect

--- src/telas/Principal/index.js
+++ src/telas/Principal/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, FlatList, TouchableOpacity } from "react-native";
 import { InformacoesUsuario } from "../../componentes/InformacoesUsuario";
 import { CardConsulta } from "../../componentes/CardConsulta";
@@ -6,30 +6,56 @@ import { TelaDeFundo } from "../../componentes/TelaDeFundo";
 import soniaFoto from "../../assets/sonia.png";
 import pacientes from "./pacientes";
 import styles from "./styles";
+import { CardConsultaShimmerEffect } from "../../componentes/CardConsultaShimmerEffect";
 
 export default function Principal({ navigation }) {
+
+  const [tempo, setTempo] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTempo(true);
+    }, 1000);
+  },[]);
 
   return (
     <TelaDeFundo>
     <View style={styles.container}>
-      <InformacoesUsuario 
+      {tempo ? 
+        <InformacoesUsuario 
         nome="Olá Sônia"
         detalhes="Mais 4 consultas hoje"
         foto={soniaFoto}
-      />
+        />
+        :
+        <CardConsultaShimmerEffect/>
+      }
+      
 
       <Text style={styles.texto}>Hoje</Text>
 
-      <FlatList
-        data={pacientes}
-        keyExtractor={item => String(item.id)}
-        renderItem={({ item }) => 
-        <TouchableOpacity onPress={() => navigation.navigate("Detalhes", item)}>
-          <CardConsulta {...item} />
-        </TouchableOpacity>
-        }
-        showsVerticalScrollIndicator={false}
-      />
+      
+
+      {tempo ?
+          <FlatList
+          data={pacientes}
+          keyExtractor={item => String(item.id)}
+          renderItem={({ item }) => 
+          <TouchableOpacity onPress={() => navigation.navigate("Detalhes", item)}>
+            <CardConsulta {...item} />
+          </TouchableOpacity>
+          }
+          showsVerticalScrollIndicator={false}
+        />
+        :
+        <>
+        <CardConsultaShimmerEffect/>
+        <CardConsultaShimmerEffect/>
+        <CardConsultaShimmerEffect/>
+        </>
+      }
+
+      
     </View> 
     </TelaDeFundo>
   );
